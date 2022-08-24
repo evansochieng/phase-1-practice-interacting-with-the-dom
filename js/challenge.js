f949af255f3ae8eaaa1c157c4c40084071601dbe
Evans Ochieng <evansochieng121@gmail.com>
2022-08-24 13:05:12 +0300

    Basic like message and switch labels

--- js/challenge.js
+++ js/challenge.js
@@ -2,35 +2,67 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // Listen to the minus button
-    const minus = document.querySelector('#minus')
-    minus.addEventListener('click', decrementCounter)
+    const minus = document.querySelector('#minus');
+    minus.addEventListener('click', decrementCounter);
 
     // Listen to the plus button
-    const plus = document.querySelector('#plus')
-    plus.addEventListener('click', incrementCounter)
+    const plus = document.querySelector('#plus');
+    plus.addEventListener('click', incrementCounter);
 
+    // Listen to the like button
+    const heart = document.querySelector('#heart');
+    heart.addEventListener('click', likeMessage);
+
+    // Listen to the pause button
+    const pause = document.querySelector('#pause');
+    pause.addEventListener('click', pauseResume);
 
     // Grab the form and listen to the form submission
     const form = document.querySelector('form');
     form.addEventListener('submit', postComment);
 
-    
 })
 
 // Decrement the counter value
 function decrementCounter() {
     // Grab the counter
-    const counter = document.querySelector('#counter')
+    const counter = document.querySelector('#counter');
     // Decrement the value by 1
-    counter.textContent = parseInt(counter.textContent, 10) - 1
+    counter.textContent = parseInt(counter.textContent, 10) - 1;
 }
 
 // Increment the counter value
 function incrementCounter() {
     // Grab the counter
-    const counter = document.querySelector('#counter')
+    const counter = document.querySelector('#counter');
     // Increment the value by 1 - First convert in to number
-    counter.textContent = parseInt(counter.textContent, 10) + 1
+    counter.textContent = parseInt(counter.textContent, 10) + 1;
+}
+
+// Add like message under the likes for a liked number
+function likeMessage() {
+    // Grab the likes section (unordered list element)
+    const likes = document.querySelector('.likes');
+    // Grab the counter
+    const counter = document.querySelector('#counter');
+
+    // Create a list element for the like message of the number liked
+    const li = document.createElement('li');
+    li.innerText = `${counter.textContent} has been liked.` //Get the value of counter
+
+    // Append the like message to the likes section
+    likes.appendChild(li);
+}
+
+// Pause and resume counter
+function pauseResume(event) {
+    // Create an object with the button labels
+    const labels = {
+        'pause': 'resume',
+        'resume': 'pause'
+    }
+    event.target.innerText = labels[event.target.innerText] //switch the label of the button
+
 }
 
 // Create a post comment function
