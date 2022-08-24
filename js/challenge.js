7422fb7a2cf8d46c03beca6582160c0d39b8a22a
Evans Ochieng <evansochieng121@gmail.com>
2022-08-24 16:01:20 +0300

    Ammended like messages

--- js/challenge.js
+++ js/challenge.js
@@ -50,6 +50,10 @@ function incrementCounter() {
     counter.textContent = parseInt(counter.textContent, 10) + 1;
 }
 
+// Declare an object to hold liked numbers, and the number of times they have been liked
+// This will assist me keep track of liked numbers
+const likedNumbers = {};
+
 // Add like message under the likes for a liked number
 function likeMessage() {
     // Grab the likes section (unordered list element)
@@ -57,12 +61,28 @@ function likeMessage() {
     // Grab the counter
     const counter = document.querySelector('#counter');
 
-    // Create a list element for the like message of the number liked
-    const li = document.createElement('li');
-    li.innerText = `${counter.textContent} has been liked.` //Get the value of counter
+    // Add like messages for the number liked
+    // Check if its the first time o
+    if (likedNumbers[counter.textContent]){ //This is not the first time the number is liked
+        likedNumbers[counter.textContent] += 1; //Increase number of liked times by 1
 
-    // Append the like message to the likes section
-    likes.appendChild(li);
+        // Grab the list item for that number using it's class
+        const li = document.querySelector(`.number${counter.textContent}`)
+        // Update the content using the current number of likes
+        li.innerText = `${counter.textContent} has been liked ${likedNumbers[counter.textContent]} times.` //Get the value of counter
+    } else { // If its the first like for a number
+        likedNumbers[counter.textContent] = 1; // Assign a value 1 to the number of times its liked
+
+        // Create a list element for the like message of the number liked
+        const li = document.createElement('li');
+        li.className = `number${counter.textContent}` //give it a class name
+
+        // Get the value of counter and the times it has been liked
+        // Add a like message as the conntent of the list element
+        li.innerText = `${counter.textContent} has been liked ${likedNumbers[counter.textContent]} times.`
+        // Append the like message to the likes section
+        likes.appendChild(li);
+    }
 }
 
 // Pause and resume counter
@@ -115,3 +135,7 @@ function postComment(event) {
     // Reset the form
     event.target.reset();
 }
+
+// YOU DID IT BOY!
+// LAKINI KARIBU ULIE, HAHAHA! STAY STRONG MAN!
+// BIG UP!
